c787f3b5a51774ad036681bf7cfca1fd32527889
Archer <archer@yaudian.com>
2020-09-24 13:32:28 +0800

    test: 尚未完成

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+// import assets from '../../../sitepackage.frontend.master/src/components/Adam/assets';
+
+function main() {
+  const filesJs = require('./files.js');
+  // console.log(assets);
+}
+
+main();
